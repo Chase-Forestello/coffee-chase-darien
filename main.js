c7a9c0962438ecf4f859e1801099e3df237237ab
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-05 09:16:04 -0500

    Incomplete - working on add coffee

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 "use strict"
-var roastSelector = document.querySelector('#roast-selection');
-var section = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var submitButton2 = document.querySelector('#submit2');
-var roastSelection = document.querySelector('#roast-selection');
+// var roastSelector = document.querySelector('#roast-selection'); I don't think we need this.
+var section = document.getElementById('coffees');
+var submitButton = document.getElementById('submit');
+var submitButton2 = document.getElementById('submit2');
+var roastSelection = document.getElementById('roast-selection');
 var names = document.getElementsByClassName("coffee");
-var selectedCreatedRoast = document.querySelector(`#createRoast`); // new roast
+var selectedCreatedRoast = document.getElementById('createRoast')
+
+console.log(`Search roast: ${roastSelection.value}`)
+console.log(`Created roast: ${selectedCreatedRoast.value}`);
+function newRoast () {
+    console.log(`Created roast: ${selectedCreatedRoast.value}`);
+}
 
 function renderCoffee(coffee) {
-    var html = `<div class="coffee col-6"><div class="fs-4">${coffee.name}</div><div class="text-muted">${coffee.roast}</div></div>`
-
-    return html;
+    return `<div class="coffee col-12 col-sm-12 col-md-12 col-lg-6 col-xxl-6">
+                    <div class="card fs-4 fw-bold text-nowrap text-center p-3 m-2 b-1"><span>${coffee.name} ${coffee.roast}</span></div>
+                </div>`;
 }
 
 function renderCoffees(coffees) {
@@ -25,24 +31,24 @@ function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
+    console.log(`Search roast: ${selectedRoast}`);
 
     if(roastSelection.value === 'all'){
         section.innerHTML = renderCoffees(coffees);
         return;
     }
-    console.log(selectedRoast);
 
     coffees.forEach(function(coffee) {
 
-        if (selectedRoast === coffee.roast) {
+        if (selectedRoast === coffee.id) {
+            // Trying to add icons to each coffee after the roast.
+            // Maybe could use set values of roast and selected roast to filter without icons causing issue otherwise need new way for icons.
             filteredCoffees.push(coffee);
         }
     });
     section.innerHTML = renderCoffees(filteredCoffees);
 
 }
-var newSelectedRoast = selectedCreatedRoast.value; // new roast
-console.log(newSelectedRoast); // new roast
 
 var searchInput = document.getElementById("coffeeName");
 
@@ -62,24 +68,26 @@ searchInput.addEventListener("keyup", (event) => {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 'light', name: 'Light City', roast: '<i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 'light', name: 'Half City', roast: '<i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 'light', name: 'Cinnamon', roast: '<i class="light fa-solid fa-mug-hot"></i>'},
+    {id: 'medium', name: 'City', roast: '<i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 'medium', name: 'American', roast: '<i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 'medium', name: 'Breakfast', roast: '<i class="medium fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'High', roast: ' <i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Continental', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'New Orleans', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'European', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Espresso', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Viennese', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'Italian', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
+    {id: 'dark', name: 'French', roast: '<i class="dark fa-solid fa-mug-hot"></i>'},
 ];
+// Remove the icons but keep the classes and try using CSS to add icons or at least style the roasts
 
 section.innerHTML = renderCoffees(coffees.reverse());
 
 submitButton.addEventListener('click', updateCoffees);
 submitButton2.addEventListener('click', updateCoffees);
-roastSelector.addEventListener('change', updateCoffees);
+roastSelection.addEventListener('change', updateCoffees);
+selectedCreatedRoast.addEventListener('change', newRoast);
